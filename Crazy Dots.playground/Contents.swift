9a4b693a76d1dac521e4d3aab144c94660598f54
Viorel <viorel.harabaru@gmail.com>
2023-02-08 21:29:28 +0200

    trying to write the export xml algorithm

--- Crazy Dots.playground/Contents.swift
+++ Crazy Dots.playground/Contents.swift
@@ -1,19 +1,33 @@
 
+import Foundation
 
-
-// DECLARE DOTS HERE with Dot()
+// MARK: DECLARE DOTS HERE with Dot()
 // PREFERABLY MOVE THEM RIGHT AWWAY with move <dotName>.moveDot(direction: )
 
 
-var dot1 = Dot(dotName:"Viorel", dotID: 0, connectTo: [0])
+var dot1 = Dot(dotName:"Test KING", dotID: 0, connectTo: [1])
+var dot2 = Dot(dotName: "King's Child", dotID: 1, connectTo: [0])
+dot2.moveDot(direction: "Down")
 
 
-
-
-// ADD YOUR DOTS INTO THE SCHEME:
+// MARK: ADD DOTS INTO THE SCHEME
 scheme = ""
 
-print(plot)
-//
+scheme.append(dot1.printDot())
+scheme.append(dot2.printDot())
+
+// MARK: Editing the file:
+ // we already have a file in the map.
+// we're telling swift to rewrite it;
+let fileManager = FileManager.default
+
+if let dir = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first {
+    let fileURL = dir.appendingPathComponent("/Users/viorelharabaru/Documents/WORKSTAGE/# PROGRAMMING/iOS DEVELOPMENT/iOS Apps & Swift Projects/Crazy Dots/test.scap")
+    do {
+        try plot.write(to: fileURL, atomically: false, encoding: .utf8)
+    } catch {
+        print(error)
+    }
+}
 
 
